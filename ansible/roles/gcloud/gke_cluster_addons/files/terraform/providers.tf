94aded97a6018ecb4bc6da2686e13c8bc20d52fd
Michal Ziemianek <mziemianek3@gmail.com>
2025-04-08 17:50:57 +0200

    big ass cleanup and refactoring xD

--- ansible/roles/gcloud/gke_cluster_addons/files/terraform/providers.tf
+++ ansible/roles/gcloud/gke_cluster_addons/files/terraform/providers.tf
@@ -31,7 +31,7 @@ provider "kubernetes" {
   host  = "https://${data.google_container_cluster.gke_cluster.endpoint}"
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate,
+    data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
   )
 }
 
@@ -41,7 +41,7 @@ provider "helm" {
     host  = "https://${data.google_container_cluster.gke_cluster.endpoint}"
     token = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(
-      data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate,
+      data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
     )
   }
 }
