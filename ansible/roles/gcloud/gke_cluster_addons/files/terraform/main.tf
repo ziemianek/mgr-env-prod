42048a70af07371392626780eab7f90d55784fb2
Michał Ziemianek <46460816+ziemianek@users.noreply.github.com>
2025-04-10 20:04:47 +0200

    Feature/gke cluster addons (#11)
    * initial setup for cluster addons
    
    * big ass cleanup and refactoring xD
    
    * another part of refactoring but addons still not working xd
    
    * change structure of cluster addons
    
    * almost every addon is deployable
    
    * addons done for now
    
    * addons done for now
    
    * small fixes

---
+++ ansible/roles/gcloud/gke_cluster_addons/files/terraform/main.tf
@@ -0,0 +1,55 @@
+# ArgoCD
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm/"
+  chart      = "argo-cd"
+  version    = "7.8.23"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+}
+
+
+resource "kubernetes_namespace" "istio_system" {
+  metadata {
+    name = "istio-system"
+  }
+}
+
+# Istio
+resource "helm_release" "istio_base" {
+  name       = "istio-base"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+  version    = "1.25.0"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+}
+
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  version    = "1.25.0"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+
+  depends_on = [helm_release.istio_base]
+}
+
+# Cert-manager
+# resource "kubernetes_namespace" "cert_manager" {
+#   metadata {
+#     name = "cert-manager"
+#   }
+# }
+
+# resource "helm_release" "cert_manager" {
+#   name       = "cert-manager"
+#   repository = "https://charts.jetstack.io"
+#   chart      = "cert-manager"
+#   version    = "v1.17.1"
+#   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
+# }
--- ansible/roles/gcloud/gke_cluster_addons/files/terraform/providers.tf
+++ ansible/roles/gcloud/gke_cluster_addons/files/terraform/providers.tf
@@ -28,16 +28,20 @@ provider "google" {
 
 # TODO: enhance provider config
 provider "kubernetes" {
-  host  = data.google_container_cluster.gke_cluster.endpoint
+  host  = "https://${data.google_container_cluster.gke_cluster.endpoint}"
   token = data.google_client_config.default.access_token
-  # cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth.cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
+  )
 }
 
 # TODO: enhance provider config
 provider "helm" {
   kubernetes {
-    host  = data.google_container_cluster.gke_cluster.endpoint
+    host  = "https://${data.google_container_cluster.gke_cluster.endpoint}"
     token = data.google_client_config.default.access_token
-    # cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth.cluster_ca_certificate)
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
+    )
   }
 }
