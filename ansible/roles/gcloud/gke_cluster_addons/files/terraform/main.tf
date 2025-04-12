b505c0b1ddd91199005b9f2f055c65ea53de19f6
Michal Ziemianek <mziemianek3@gmail.com>
2025-04-12 19:41:30 +0200

    some fixes for cert manager and istio (istio still not working correctly)

--- ansible/roles/gcloud/gke_cluster_addons/files/terraform/main.tf
+++ ansible/roles/gcloud/gke_cluster_addons/files/terraform/main.tf
@@ -39,17 +39,34 @@ resource "helm_release" "istiod" {
   depends_on = [helm_release.istio_base]
 }
 
-# Cert-manager
-# resource "kubernetes_namespace" "cert_manager" {
-#   metadata {
-#     name = "cert-manager"
-#   }
-# }
+resource "helm_release" "istio_ingressgateway" {
+  name       = "istio-ingressgateway"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "gateway"
+  version    = "1.25.0"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
 
-# resource "helm_release" "cert_manager" {
-#   name       = "cert-manager"
-#   repository = "https://charts.jetstack.io"
-#   chart      = "cert-manager"
-#   version    = "v1.17.1"
-#   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
-# }
+  depends_on = [helm_release.istiod]
+
+  #   values = [
+  #     <<EOF
+  # service:
+  #   type: LoadBalancer
+  # EOF
+  #   ]
+}
+
+# Cert-manager
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.17.1"
+  namespace  = kubernetes_namespace.cert_manager.metadata[0].name
+}
