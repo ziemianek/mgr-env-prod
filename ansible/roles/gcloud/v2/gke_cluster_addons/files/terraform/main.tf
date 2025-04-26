7d0d2d38ac5d76e9e8a6c83ee5b391cf30a39bfa
Michal Ziemianek <mziemianek3@gmail.com>
2025-04-26 17:26:57 +0200

    sam nie pamietam juz co robilem XD

---
+++ ansible/roles/gcloud/v2/gke_cluster_addons/files/terraform/main.tf
@@ -0,0 +1,51 @@
+resource "helm_release" "istio_base" {
+  name      = "istio-base"
+  chart     = var.istio_base
+  namespace = "istio-system"
+
+  create_namespace = true
+
+  set {
+    name  = "global.istioNamespace"
+    value = "istio-system"
+  }
+}
+
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  chart      = var.istiod
+  namespace  = helm_release.istio_base.namespace
+  depends_on = [helm_release.istio_base]
+
+  set {
+    name  = "global.istioNamespace"
+    value = "istio-system"
+  }
+
+  set {
+    name  = "telemetry.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "meshConfig.ingressService"
+    value = "istio-ingress-gateway"
+  }
+}
+
+resource "helm_release" "istio_ingress_gateway" {
+  name       = "istio-ingress-gateway"
+  chart      = var.istio_gateway
+  namespace  = "istio-ingress"
+  depends_on = [helm_release.istiod]
+
+  create_namespace = true
+}
+
+resource "helm_release" "cert_manager" {
+  name      = "cert-manager"
+  chart     = var.cert_manager
+  namespace = "cert-manager"
+
+  create_namespace = true
+}
